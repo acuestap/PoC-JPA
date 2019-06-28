705c3cae8c6049b8f6854233e62475f57f67ca58
Abimelec Cuesta <acuestap@gmail.com>
2019-06-28 17:38:18 -0500

    Commit Inicial del ejemploTransactionalJPA

---
+++ PoC-TransactionalJPA/src/main/java/com/acpdev/poc/jpa/PoCTransactionalJpaApplication.java
@@ -0,0 +1,13 @@
+package com.acpdev.poc.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoCTransactionalJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoCTransactionalJpaApplication.class, args);
+	}
+
+}
